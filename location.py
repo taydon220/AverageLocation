1fc59db530be5006ee533386ed8b264aa20de092
taydon220 <taydon220@gmail.com>
2020-09-11 12:06:17 -0500

    Prints out locations used and their info. Asks to open map url.

--- location.py
+++ location.py
@@ -5,3 +5,8 @@ class Location:
         self.lon = lon
         self.weight = weight
         self.coords = (lat, lon)
+
+    def print_info(self):
+        print(f"Name: {self.name}")
+        print(f"Coordinates: {self.coords}")
+        print(f"Weight: {self.weight} years.\n")
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import webbrowser
-from config import my_locations
+import config
 from center_geolocation import find_weighted_center, find_center
 
 
 if __name__ == "__main__":
     coords = []
-    for location in my_locations:
+    for c, location in enumerate(config.my_locations, 1):
+        print(f"Location #{c}:")
+        location.print_info()
         coords.append(location.coords)
     center = find_center(coords)
-    weighted_center = find_weighted_center(my_locations)
-    print(f"Your un-weighted center coordinates are: {center}")
-    print(f"Your weighted center coordinates are: {weighted_center}")
-    webbrowser.open(f"https://www.google.com/maps/search/{center}")
-    webbrowser.open(f"https://www.google.com/maps/search/{weighted_center}")
+    weighted_center = find_weighted_center(config.my_locations)
+    print(f"Un-weighted center: {center}")
+    print(f"Weighted center: {weighted_center}")
+    print("")
+    center_map = input("Would you like to see the maps? (Y)")
+    if center_map.lower() == "y":
+        webbrowser.open(f"https://www.google.com/maps/search/{center}")
+        webbrowser.open(f"https://www.google.com/maps/search/{weighted_center}")
+
